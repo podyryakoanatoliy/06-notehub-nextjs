6137725fc1e4b1feed424e2aadd77dd486dd8e2b
Anatoliy Podyryako <podyryakoanatoliy@gmail.com>
2025-11-28 17:02:56 +0200

    replace loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,5 @@
 "use client";
 // import { BallTriangle } from "react-loader-spinner";
 export default function Loading() {
-  return <p>Loading, please wait...</p>;
+  return <p>Loading, please wait.....</p>;
 }
